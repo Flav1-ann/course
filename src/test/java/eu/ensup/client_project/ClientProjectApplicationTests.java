494fb28c306bca4a3f68b50ed6ae4ddec5834a98
Flavien <monstro28500@gmail.com>
2022-03-15 09:48:01 +0100

    first commit

---
+++ src/test/java/eu/ensup/client_project/ClientProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package eu.ensup.client_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
